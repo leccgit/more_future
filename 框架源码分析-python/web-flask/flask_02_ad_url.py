7bfc7245ca32b4db1ac60f37ba5848e2960b820b
leichao <lei15827494582cc@163.com>
2021-08-02 15:16:32 +0800

    flask 源码学习: 添加路由分发器

--- 框架源码分析-python/web-flask/flask_02_ad_url.py
+++ 框架源码分析-python/web-flask/flask_02_ad_url.py
@@ -1,6 +1,6 @@
-import time
-from pprint import pprint
-from werkzeug import Response as BaseResponse
+from werkzeug import Response as BaseResponse, Request as RequestBase
+from werkzeug.routing import Map, Rule
+from werkzeug.exceptions import HTTPException
 
 
 class Response(BaseResponse):
@@ -14,24 +14,41 @@ class Flask_02(object):
     def __init__(self):
         self.wsgi_app = self.wsgi_app
         self.view_functions = {}  # 路由装饰函数
+        self.url_map = Map()
+        self.error_handlers = {}
 
-    def route(self, rule, endpoint, **options):
+    def errorHandlers(self, code):
         """
-        添加路由装饰器
+        添加指定状态码的错误处理
+        :param code:
+        :return:
+        """
+
+        def decorator(f):
+            self.error_handlers[code] = f
+
+        return decorator
+
+    def route(self, rule, **options):
+        """
+        添加路由装饰器和路由装饰函数
         等价于 self.view_function[] = xx
         :param rule:
-        :param endpoint:
         :param options:
         :return:
         """
+
         def decorator(f):
-            self.add_url(rule, endpoint, **options)
+            self.add_url_rule(rule, f.__name__, **options)
+            self.view_functions[f.__name__] = f
             return f
 
         return decorator
 
-    def add_url(self, rule, endpoint, **options):
-        pass
+    def add_url_rule(self, rule, endpoint, **options):
+        options['endpoint'] = endpoint
+        options.setdefault('methods', ('GET',))
+        self.url_map.add(Rule(rule, **options))
 
     def run(self, host='127.0.0.1', port=8080, **options):
         from werkzeug import run_simple
@@ -44,28 +61,45 @@ class Flask_02(object):
         :param start_response: 在可调用对象中调用的函数，用来发起响应，参数包括状态码，headers等
         :return:
         """
-        pprint(environ)
-        return_body = ["\nHello WSGI!"]  # 返回结果必须是bytes
-        response = self.make_response(*return_body)
+        url_adapter = self.url_map.bind_to_environ(environ)
+        try:
+            # 通过环境变量,找到对应的视图函数,并进行执行
+            endpoint, values = url_adapter.match()
+            result = self.view_functions[endpoint](**values)
+        except HTTPException as e:
+            handler = self.error_handlers.get(e.code)
+            if handler is None:
+                result = e
+            else:
+                result = handler(e)
+        response = self.make_response(result, environ)
         return response(environ, start_response)
 
-    def make_response(self, rv):
+    def make_response(self, rv, environ):
         if isinstance(rv, str):
             return self.response_class(rv)
         if isinstance(rv, tuple):
             return self.response_class(*rv)
-        print('参数错误！')
+        return self.response_class.force_type(rv, environ)
 
     def __call__(self, environ, start_response):
-        # start_time = time.time()
-        # # response =
-        # # pprint(response)
-        # response_time = (time.time() - start_time) * 1000
-        # timing_text = "记录请求耗时中间件输出\n\n本次请求耗时: {:.10f}ms \n\n\n".format(response_time)
-        # response.append(timing_text.encode('utf-8'))
         return self.wsgi_app(environ, start_response)
 
 
 if __name__ == '__main__':
     app = Flask_02()
+
+
+    @app.errorHandlers(404)
+    def page_note_found(error):
+        return "This page does not exist', 404"
+
+
+    @app.route('/name')
+    def make_index():
+        print('this is index test!')
+        return 'make_index'
+
+
     app.run()
+    print(app.url_map)
