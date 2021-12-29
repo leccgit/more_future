4ac89e87f4ac2168d681d45881c565eee690ee7e
leichao <lei15827494582cc@163.com>
2021-12-29 08:53:22 +0800

    feat: 调整ftp_clint格式代码

--- 编程实践/ftp_study/ftp_client/mg_ftp_client.py
+++ 编程实践/ftp_study/ftp_client/mg_ftp_client.py
@@ -1,4 +1,6 @@
 import os
+import time
+from typing import Tuple
 from ftplib import FTP, error_perm
 
 
@@ -7,7 +9,7 @@ class FtpFileNotFound(Exception):
 
 
 class FtpConnParams:
-    # 构建ftp的连接参数
+    # ftp的连接参数
     def __init__(self):
         self.host = ""
         self.port = 21
@@ -19,6 +21,9 @@ class FtpConnParams:
 def build_ftp_conn_params(
         host="", port=21, user="", passwd="", timeout=10
 ) -> FtpConnParams:
+    assert host, "ftp host is None!"
+
+    # 构建ftp的连接参数
     conn_params = FtpConnParams()
     conn_params.host = host
     conn_params.port = int(port)
@@ -29,11 +34,11 @@ def build_ftp_conn_params(
     return conn_params
 
 
-def build_ftp_connection(
+def create_ftp_conn_params(
         ftp_client: FTP,
         conn_params: FtpConnParams
 ) -> FTP:
-    # 构建ftp连接参数
+    # 建立ftp连接
     ftp_client.connect(
         host=conn_params.host,
         port=conn_params.port,
@@ -48,10 +53,11 @@ def build_ftp_connection(
 
 def is_ftp_file(
         ftp_client: FTP,
-        file_name: str,
-        ftp_path: str
+        remote_file: str,
 ) -> bool:
     # 校验指定文件名，在远端的ftp服务器中是否存在
+    file_name = os.path.basename(remote_file)
+    ftp_path = os.path.dirname(remote_file)
     try:
         if file_name in ftp_client.nlst(ftp_path):
             return True
@@ -61,43 +67,57 @@ def is_ftp_file(
         return False
 
 
-def ftp_down_load_file(
+def ftp_download_remote_file_with_time(
         conn_params: FtpConnParams,
-        ftp_file: str,
+        remote_file: str,
+        local_file: str = "",
+        buf_size=1024,
+        time_out=10
+) -> Tuple[bool, str]:
+    start_time = time.time()
+    save_local_file = ftp_download_remote_file(
+        conn_params, remote_file, local_file=local_file, buf_size=buf_size)
+    end_time = time.time()
+    if end_time - start_time > time_out:
+        return True, save_local_file
+    return False, save_local_file
+
+
+def ftp_download_remote_file(
+        conn_params: FtpConnParams,
+        remote_file: str,
         local_file: str = "",
         buf_size=1024
-) -> None:
+) -> str:
     """
     通过ftp读取远端的文件，并保存到本地
     :param conn_params:
-    :param ftp_file: 远程文件路径
-    :param local_file: 本地文件路径
+    :param remote_file: 远程文件路径
+    :param local_file: 本地文件路径, 默认为空, 将文件保存到当前文件夹下
     :param buf_size: 单次读取文件buf, 默认1kb
     :return:
     """
-    # 构建文件参数
-    ftp_file_name = os.path.basename(ftp_file)
-    ftp_file_dir = os.path.dirname(ftp_file)
+    assert type(conn_params) is FtpConnParams, "conn_params type error!"
     if not local_file:
-        local_file_name = ftp_file_name
-    else:
-        local_file_name = os.path.basename(local_file)
-    local_file_path = os.path.join(os.path.dirname(__file__), local_file_name)
-
+        local_file = os.path.join(os.path.dirname(__file__), os.path.basename(remote_file))
     # 创建连接参数
-    with FTP() as ftp_client:
-        # 建立ftp客户端的连接
-        build_ftp_connection(ftp_client, conn_params)
+    with FTP() as fp:
+        create_ftp_conn_params(fp, conn_params)
         # 校验远端的文件是否存在
-        if not is_ftp_file(ftp_client, ftp_file_name, ftp_file_dir):
-            raise FtpFileNotFound("ftp file not found,"
-                                  " file:{} not exits!".format(ftp_file))
+        if not is_ftp_file(fp, remote_file):
+            raise FtpFileNotFound("ftp file not found, filename:{}".format(remote_file))
         # 读取文件保存到本地
-        with open(local_file_path, "wb") as f:
-            ftp_client.retrbinary(
-                'RETR %s' % ftp_file, f.write, buf_size)
+        with open(local_file, "wb") as f:
+            fp.retrbinary(
+                'RETR %s' % remote_file, f.write, buf_size)
     print("down load suc......")
+    print("download file path: {}".format(local_file))
+    return local_file
 
 
 if __name__ == '__main__':
-    pass
+    save_local_path = ftp_download_remote_file_with_time(build_ftp_conn_params(
+        host="xxxx",
+        user="xxx",
+        passwd="xxxx"
+    ), r"is_test/test.mdb")
