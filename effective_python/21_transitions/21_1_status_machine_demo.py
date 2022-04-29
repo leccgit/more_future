0f2e738f91313829755fccf5923fa94bbb4dbef1
leichao <lei15827494582cc@163.com>
2022-04-29 10:03:13 +0800

    feat: 添加状态机的测试

--- effective_python/21_transitions/21_1_status_machine_demo.py
+++ effective_python/21_transitions/21_1_status_machine_demo.py
@@ -1,68 +1,27 @@
-import random
 from transitions import Machine
 
 
-class NarcolepticSuperhero(object):
-    # 定义一组状态集合
-    states = ['asleep', 'hanging out', 'hungry', 'sweaty', 'saving the world']
+class Matter(object):
 
-    def __init__(self, name):
-        self.name = name
+    def __init__(self):
+        self.temp = 0
+        self.pressure = 101.325
 
-        # What have we accomplished today?
-        self.kittens_rescued = 0
+    # Note that the sole argument is now the EventData instance.
+    # This object stores positional arguments passed to the trigger method in the
+    # .args property, and stores keywords arguments in the .kwargs dictionary.
+    def set_environment(self, event):
+        self.temp = event.kwargs.get('temp', 0)
+        self.pressure = event.kwargs.get('pressure', 101.325)
 
-        # 初始化状态机
-        self.machine = Machine(model=self, states=NarcolepticSuperhero.states, initial='asleep')
-
-        # Add some transitions. We could also define these using a static list of
-        # dictionaries, as we did with states above, and then pass the list to
-        # the Machine initializer as the transitions= argument.
-
-        # At some point, every superhero must rise and shine.
-        self.machine.add_transition(trigger='wake_up', source='asleep', dest='hanging out')
-
-        # Superheroes need to keep in shape.
-        self.machine.add_transition('work_out', 'hanging out', 'hungry')
-
-        # Those calories won't replenish themselves!
-        self.machine.add_transition('eat', 'hungry', 'hanging out')
-
-        # Superheroes are always on call. ALWAYS. But they're not always
-        # dressed in work-appropriate clothing.
-        self.machine.add_transition('distress_call', '*', 'saving the world',
-                                    before='change_into_super_secret_costume')
-
-        # When they get off work, they're all sweaty and disgusting. But before
-        # they do anything else, they have to meticulously log their latest
-        # escapades. Because the legal department says so.
-        self.machine.add_transition('complete_mission', 'saving the world', 'sweaty',
-                                    after='update_journal')
-
-        # Sweat is a disorder that can be remedied with water.
-        # Unless you've had a particularly long day, in which case... bed time!
-        self.machine.add_transition('clean_up', 'sweaty', 'asleep', conditions=['is_exhausted'])
-        self.machine.add_transition('clean_up', 'sweaty', 'hanging out')
-
-        # Our NarcolepticSuperhero can fall asleep at pretty much any time.
-        self.machine.add_transition('nap', '*', 'asleep')
-
-    def update_journal(self):
-        """ Dear Diary, today I saved Mr. Whiskers. Again. """
-        self.kittens_rescued += 1
-
-    @property
-    def is_exhausted(self):
-        """ Basically a coin toss. """
-        return random.random() < 0.5
-
-    def change_into_super_secret_costume(self):
-        print("Beauty, eh?")
+    def print_pressure(self): print("Current pressure is %.2f kPa." % self.pressure)
 
 
-if __name__ == '__main__':
-    batman = NarcolepticSuperhero("Batman")
-    print(batman.state)
-    batman.wake_up()
-    print(batman.state)
+lump = Matter()
+machine = Machine(lump, ['solid', 'liquid'], send_event=True, initial='solid')
+machine.add_transition('melt', 'solid', 'liquid', before='set_environment')
 
+print(lump.state)
+lump.melt(temp=45, pressure=1853.68)  # keyword args
+lump.print_pressure()
+print(lump.state)
